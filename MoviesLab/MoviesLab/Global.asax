1f0da06570ac466cf7768c2f68e3965502201b62
Svyatoslav Solopchenko <solopchenko@Svyatoslavs-MacBook-Air.local>
2015-08-20 11:15:33 +0300

    Create a new project

---
+++ MoviesLab/MoviesLab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesLab.MvcApplication" Language="C#" %>
---
+++ MoviesLab/MoviesLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesLab.Startup))]
+namespace MoviesLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
